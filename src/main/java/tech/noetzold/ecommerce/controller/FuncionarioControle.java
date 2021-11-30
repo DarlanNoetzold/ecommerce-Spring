6016b48cc3fcd665d73f917a4d68028fa0417ada
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-11-30 08:00:15 -0300

    Create FuncionarioControle.java

---
+++ src/main/java/tech/noetzold/ecommerce/controller/FuncionarioControle.java
@@ -0,0 +1,71 @@
+package tech.noetzold.ecommerce.controller;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import tech.noetzold.ecommerce.model.Funcionario;
+import tech.noetzold.ecommerce.repository.CidadeRepositorio;
+import tech.noetzold.ecommerce.repository.FuncionarioRepositorio;
+
+@Controller
+public class FuncionarioControle {
+	
+	@Autowired
+	private FuncionarioRepositorio funcionarioRepositorio;
+	
+	@Autowired
+	private CidadeRepositorio cidadeRepositorio;
+	
+	
+	@GetMapping("/administrativo/funcionarios/cadastrar")
+	public ModelAndView cadastrar(Funcionario funcionario) {
+		ModelAndView mv =  new ModelAndView("administrativo/funcionarios/cadastro");
+		mv.addObject("funcionario",funcionario);
+		mv.addObject("listaCidades",cidadeRepositorio.findAll());
+		return mv;
+	}
+	
+	@GetMapping("/administrativo/funcionarios/listar")
+	public ModelAndView listar() {
+		ModelAndView mv=new ModelAndView("administrativo/funcionarios/lista");
+		mv.addObject("listaFuncionarios", funcionarioRepositorio.findAll());
+		return mv;
+	}
+	
+	@GetMapping("/administrativo/funcionarios/editar/{id}")
+	public ModelAndView editar(@PathVariable("id") Long id) {
+		Optional<Funcionario> funcionario = funcionarioRepositorio.findById(id);
+		return cadastrar(funcionario.get());
+	}
+	
+	@GetMapping("/administrativo/funcionarios/remover/{id}")
+	public ModelAndView remover(@PathVariable("id") Long id) {
+		Optional<Funcionario> funcionario = funcionarioRepositorio.findById(id);
+		funcionarioRepositorio.delete(funcionario.get());
+		return listar();
+	}
+	
+	@PostMapping("/administrativo/funcionarios/salvar")
+	public ModelAndView salvar(@Valid Funcionario funcionario, BindingResult result) {
+		
+		//System.out.println(result.getAllErrors());
+		if(result.hasErrors()) {
+			return cadastrar(funcionario);
+		}
+		funcionario.setSenha(new BCryptPasswordEncoder().encode(funcionario.getSenha()));
+		funcionarioRepositorio.saveAndFlush(funcionario);
+		
+		return cadastrar(new Funcionario());
+	}
+
+}
