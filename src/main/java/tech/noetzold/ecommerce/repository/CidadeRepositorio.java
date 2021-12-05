d18e6fcc7ca894cb16f44a4a96be676e8bad7d79
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-05 08:21:56 -0300

    Refatoração

--- src/main/java/tech/noetzold/ecommerce/repository/CidadeRepositorio.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tech.noetzold.ecommerce.model.Cidade;
-
-public interface CidadeRepositorio extends JpaRepository<Cidade, Long> {
-
-}
--- src/main/java/tech/noetzold/ecommerce/repository/FuncionarioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tech.noetzold.ecommerce.model.Funcionario;
-
-public interface FuncionarioRepositorio extends JpaRepository<Funcionario, Long> {
-
-}
