5c6fad1d3a46c5d360f676ccc5bec4bdc917a91d
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-19 07:38:09 -0300

    Create CarrinhoControle.java

---
+++ src/main/java/tech/noetzold/ecommerce/controle/CarrinhoControle.java
@@ -0,0 +1,177 @@
+package tech.noetzold.ecommerce.controle;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import tech.noetzold.ecommerce.modelos.Cliente;
+import tech.noetzold.ecommerce.modelos.Compra;
+import tech.noetzold.ecommerce.modelos.ItensCompra;
+import tech.noetzold.ecommerce.modelos.Produto;
+import tech.noetzold.ecommerce.modelos.Produto;
+import tech.noetzold.ecommerce.repositorios.ClienteRepositorio;
+import tech.noetzold.ecommerce.repositorios.CompraRepositorio;
+import tech.noetzold.ecommerce.repositorios.EstadoRepositorio;
+import tech.noetzold.ecommerce.repositorios.FuncionarioRepositorio;
+import tech.noetzold.ecommerce.repositorios.ItensCompraRepositorio;
+import tech.noetzold.ecommerce.repositorios.ProdutoRepositorio;
+
+@Controller
+public class CarrinhoControle {
+
+	private List<ItensCompra> itensCompra = new ArrayList<ItensCompra>();
+	private Compra compra = new Compra();
+	private Cliente cliente;
+
+	@Autowired
+	private ProdutoRepositorio repositorioProduto;
+
+	@Autowired
+	private ClienteRepositorio repositorioCliente;
+
+	@Autowired
+	private CompraRepositorio repositorioCompra;
+
+	@Autowired
+	private ItensCompraRepositorio repositorioItensCompra;
+
+	private void calcularTotal() {
+		compra.setValorTotal(0.);
+		for (ItensCompra it : itensCompra) {
+			compra.setValorTotal(compra.getValorTotal() + it.getValorTotal());
+		}
+	}
+
+	@GetMapping("/carrinho")
+	public ModelAndView chamarCarrinho() {
+		ModelAndView mv = new ModelAndView("cliente/carrinho");
+		calcularTotal();
+		// System.out.println(compra.getValorTotal());
+		mv.addObject("compra", compra);
+		mv.addObject("listaItens", itensCompra);
+		return mv;
+	}
+
+	private void buscarUsuarioLogado() {
+		Authentication autenticado = SecurityContextHolder.getContext().getAuthentication();
+		if (!(autenticado instanceof AnonymousAuthenticationToken)) {
+			String email = autenticado.getName();
+			cliente = repositorioCliente.buscarClienteEmail(email).get(0);
+		}
+	}
+
+	@GetMapping("/finalizar")
+	public ModelAndView finalizarCompra() {
+		buscarUsuarioLogado();
+		ModelAndView mv = new ModelAndView("cliente/finalizar");
+		calcularTotal();
+		// System.out.println(compra.getValorTotal());
+		mv.addObject("compra", compra);
+		mv.addObject("listaItens", itensCompra);
+		mv.addObject("cliente", cliente);
+		return mv;
+	}
+
+	@PostMapping("/finalizar/confirmar")
+	public ModelAndView confirmarCompra(String formaPagamento) {
+		ModelAndView mv = new ModelAndView("cliente/mensagemFinalizou");
+		compra.setCliente(cliente);
+		compra.setFormaPagamento(formaPagamento);
+		repositorioCompra.saveAndFlush(compra);
+
+		for (ItensCompra c : itensCompra) {
+			c.setCompra(compra);
+			repositorioItensCompra.saveAndFlush(c);
+		}
+		itensCompra = new ArrayList<>();
+		compra = new Compra();
+		return mv;
+	}
+
+	@GetMapping("/alterarQuantidade/{id}/{acao}")
+	public String alterarQuantidade(@PathVariable Long id, @PathVariable Integer acao) {
+		ModelAndView mv = new ModelAndView("cliente/carrinho");
+
+		for (ItensCompra it : itensCompra) {
+			if (it.getProduto().getId().equals(id)) {
+				// System.out.println(it.getValorTotal());
+				if (acao.equals(1)) {
+					it.setQuantidade(it.getQuantidade() + 1);
+					it.setValorTotal(0.);
+					it.setValorTotal(it.getValorTotal() + (it.getQuantidade() * it.getValorUnitario()));
+				} else if (acao == 0) {
+					it.setQuantidade(it.getQuantidade() - 1);
+					it.setValorTotal(0.);
+					it.setValorTotal(it.getValorTotal() + (it.getQuantidade() * it.getValorUnitario()));
+				}
+				break;
+			}
+		}
+
+		return "redirect:/carrinho";
+	}
+
+	@GetMapping("/removerProduto/{id}")
+	public String removerProdutoCarrinho(@PathVariable Long id) {
+
+		for (ItensCompra it : itensCompra) {
+			if (it.getProduto().getId().equals(id)) {
+				itensCompra.remove(it);
+				break;
+			}
+		}
+
+		return "redirect:/carrinho";
+	}
+
+	@GetMapping("/adicionarCarrinho/{id}")
+	public String adicionarCarrinho(@PathVariable Long id) {
+
+		Optional<Produto> prod = repositorioProduto.findById(id);
+		Produto produto = prod.get();
+
+		int controle = 0;
+		for (ItensCompra it : itensCompra) {
+			if (it.getProduto().getId().equals(produto.getId())) {
+				it.setQuantidade(it.getQuantidade() + 1);
+				it.setValorTotal(0.);
+				it.setValorTotal(it.getValorTotal() + (it.getQuantidade() * it.getValorUnitario()));
+				controle = 1;
+				break;
+			}
+		}
+		if (controle == 0) {
+			ItensCompra item = new ItensCompra();
+			item.setProduto(produto);
+			item.setValorUnitario(produto.getValorVenda());
+			item.setQuantidade(item.getQuantidade() + 1);
+			item.setValorTotal(item.getValorTotal() + (item.getQuantidade() * item.getValorUnitario()));
+
+			itensCompra.add(item);
+		}
+
+		return "redirect:/carrinho";
+	}
+
+}
