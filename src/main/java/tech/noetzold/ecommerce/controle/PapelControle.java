0839fe94bd574a28a6c5481b6c6a829dd6d061e7
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-28 07:21:35 -0300

    Create PapelControle.java

---
+++ src/main/java/tech/noetzold/ecommerce/controle/PapelControle.java
@@ -0,0 +1,65 @@
+package tech.noetzold.ecommerce.controle;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import tech.noetzold.ecommerce.modelos.Papel;
+import tech.noetzold.ecommerce.modelos.Papel;
+import tech.noetzold.ecommerce.repositorios.PapelRepositorio;
+import tech.noetzold.ecommerce.repositorios.FuncionarioRepositorio;
+
+@Controller
+public class PapelControle {
+	
+	@Autowired
+	private PapelRepositorio papelRepositorio;
+	
+	
+	@GetMapping("/administrativo/papeis/cadastrar")
+	public ModelAndView cadastrar(Papel papel) {
+		ModelAndView mv =  new ModelAndView("administrativo/papeis/cadastro");
+		mv.addObject("papel",papel);
+		return mv;
+	}
+	
+	@GetMapping("/administrativo/papeis/listar")
+	public ModelAndView listar() {
+		ModelAndView mv=new ModelAndView("administrativo/papeis/lista");
+		mv.addObject("listaPapeis", papelRepositorio.findAll());
+		return mv;
+	}
+	
+	@GetMapping("/administrativo/papeis/editar/{id}")
+	public ModelAndView editar(@PathVariable("id") Long id) {
+		Optional<Papel> papel = papelRepositorio.findById(id);
+		return cadastrar(papel.get());
+	}
+	
+	@GetMapping("/administrativo/papeis/remover/{id}")
+	public ModelAndView remover(@PathVariable("id") Long id) {
+		Optional<Papel> papel = papelRepositorio.findById(id);
+		papelRepositorio.delete(papel.get());
+		return listar();
+	}
+	
+	@PostMapping("/administrativo/papeis/salvar")
+	public ModelAndView salvar(@Valid Papel papel, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return cadastrar(papel);
+		}
+		papelRepositorio.saveAndFlush(papel);
+		
+		return cadastrar(new Papel());
+	}
+
+}
