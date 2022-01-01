55b1d6a4ac15d62a92f9a89e0e94d26b61d8a1ff
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-01-01 06:52:12 -0300

    Create SecurityAdministrativo.java

---
+++ src/main/java/tech/noetzold/ecommerce/SecurityAdministrativo.java
@@ -0,0 +1,50 @@
+package tech.noetzold.ecommerce;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@Order(2)
+public class SecurityAdministrativo extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// neste método que vamos tratar os usuários
+
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery(
+						"select email as username, senha as password, 1 as enable from funcionario where email=?")
+				.authoritiesByUsernameQuery(
+						"select funcionario.email as username, papel.nome as authority from permissoes inner join funcionario on funcionario.id=permissoes.funcionario_id inner join papel on permissoes.papel_id=papel.id where funcionario.email=?")
+				.passwordEncoder(new BCryptPasswordEncoder());
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/login").permitAll().antMatchers("/administrativo/cadastrar/**")
+				.hasAnyAuthority("gerente").antMatchers("/administrativo/**").authenticated().and().formLogin()
+				.loginPage("/login").failureUrl("/login").loginProcessingUrl("/admin")
+				.defaultSuccessUrl("/administrativo").usernameParameter("username").passwordParameter("password").and()
+				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/administrativo/logout"))
+				.logoutSuccessUrl("/login").deleteCookies("JSESSIONID").and().exceptionHandling()
+				.accessDeniedPage("/negado").and().csrf().disable();
+
+	}
+
+}
