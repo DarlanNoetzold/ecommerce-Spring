2c69e8f80f828601d1e20f03a5596de456da52a4
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-01-02 06:59:59 -0300

    Create DataConfiguration.java

---
+++ src/main/java/tech/noetzold/ecommerce/DataConfiguration.java
@@ -0,0 +1,24 @@
+package com.dev.loja;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc.postgresql://localhost:5432/ecommerce");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("admin");
+		return dataSource;
+	}
+}
