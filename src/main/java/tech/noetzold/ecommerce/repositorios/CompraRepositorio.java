9eecee17d58181975b4138cfb260fcd4410cd8f7
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-01-15 08:10:06 -0300

    Create CompraRepositorio.java

---
+++ src/main/java/tech/noetzold/ecommerce/repositorios/CompraRepositorio.java
@@ -0,0 +1,9 @@
+package tech.noetzold.ecommerce.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tech.noetzold.ecommerce.modelos.Compra;
+
+public interface CompraRepositorio extends JpaRepository<Compra, Long> {
+
+}
